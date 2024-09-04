12ee52a27560dc5222fbfceb7a15c1c76cda8cd8
Andrew <andystep333@gmail.com>
2024-09-04 23:56:31 +0300

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-// import { Title } from "./title";
 import { TopBar } from "./top-bar";
 
 export const Header: React.FC = () => {
   return (
     <header>
-      {/* <Title /> */}
       <TopBar />
     </header>
   );
